c71aa672bdd4d2ad7ed39f986538bd1b60ec77b4
mayank1112 <35667550+mayank1112@users.noreply.github.com>
2018-03-29 18:31:14 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,6 @@
+git clone http://Srivastat@stash.cdk.com/scm/arirel/autoline-drive-custom-application.git
+git remote add origin http://Srivastat@stash.cdk.com/scm/arirel/autoline-drive-custom-application.git
+git checkout feature/react-component
+git add .
+git commit -m "build"
+git push origin 
